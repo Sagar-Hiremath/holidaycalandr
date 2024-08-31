8c8e52934deb1ed2d5949864ad469fc08506f9ba
Sagar <hiremathsagar135@gmail.com>
2024-08-31 12:45:54 +0530

    Initial commit

---
+++ src/main/java/com/publicholiday/holidaycalandr/HolidaycalandrApplication.java
@@ -0,0 +1,13 @@
+package com.publicholiday.holidaycalandr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidaycalandrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidaycalandrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/publicholiday/holidaycalandr/HolidaycalandrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicholiday.holidaycalandr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidaycalandrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
